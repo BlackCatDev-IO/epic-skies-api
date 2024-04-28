271ad2423ed3d0c91bc8c44a8ca7a9be92bff492
Loren Aguey <loren@blackcatdev.io>
2024-04-28 11:26:24 -0400

    Add local mongo connection string

--- config/config.py
+++ config/config.py
@@ -3,6 +3,7 @@ from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
     MONGO_URL: str
+    MONGO_URL_LOCAL: str
     CONFIG_ID: str
     CURRENT_ALERTS_LIST_ID: str
     SENTRY_URL: str
