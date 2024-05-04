bb0f327deaf7507b47e9a581eb0c1f88faf03f20
Loren Aguey <loren@blackcatdev.io>
2024-05-03 23:16:12 -0400

    Check for local host before setting db connection string

--- infrastructure/mongo_setup.py
+++ infrastructure/mongo_setup.py
@@ -8,11 +8,23 @@ from models.current_alerts_list_model import CurrentAlertsList
 from models.log_model import LogModel
 from models.mock_response_model import MockResponseModel
 from models.user_model import UserModel
+import socket
+
+
+def is_local_host() -> bool:
+    try:
+        hostname = socket.gethostname()
+        # gethostname returns computer name on Macs
+        return "Loren" in hostname
+    except Exception as e:
+        print(e)
+        return False
 
 
 async def init_connection(db_name: str):
     try:
-        client = motor.motor_asyncio.AsyncIOMotorClient(settings.MONGO_URL)
+        connection_string = settings.MONGO_URL_LOCAL if is_local_host() else settings.MONGO_URL
+        client = motor.motor_asyncio.AsyncIOMotorClient(connection_string)
         model_list = [UserModel, ConfigModel, CurrentAlertsList, AppAlertNotice, LogModel, MockResponseModel]
 
         await beanie.init_beanie(database=client[db_name], document_models=model_list)
